5b3d8d737e9efd6461625cadf2d3522b9a145dd1
etinajero054 <117051826+etinajero054@users.noreply.github.com>
2022-12-28 21:35:57 -0600

    Function examples

---
+++ FunctionExample/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void myfunc(void);
+int mysum(int a, int b);
+
+int main(int argc, char** argv)
+{
+    myfunc();
+    int result = mysum(10,5);
+    printf("%i + %i = %i", 10, 5, result);
+    return 0;
+}
+
+void myfunc(void)
+{
+    printf("My func called\n");
+}
+
+int mysum(int a, int b)
+{
+    return a + b;
+}
